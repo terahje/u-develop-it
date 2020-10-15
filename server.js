9580529d8a386e74567bd144a22a76e2502520f1
terahje <terahjequinn@gmail.com>
2020-10-14 21:58:08 -0700

    can now delete candidate

--- server.js
+++ server.js
@@ -16,6 +16,73 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database.');
   });
 
+
+
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });  
+  
+// Get single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+  
+      res.json({
+        message: 'successfully deleted',
+        changes: this.changes
+      });
+    });
+  });
+
+// Create a candidate
+//const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//VALUES (?,?,?,?)`;
+//const params = [1, 'Ronald', 'Firbank', 1];
+// ES5 function, not arrow function, to use this/
+//db.run(sql, params, function(err, result) {
+//if (err) {
+//console.log(err);//
+//}
+//console.log(result, this.lastID);
+//});
+
+
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
     res.status(404).end();
